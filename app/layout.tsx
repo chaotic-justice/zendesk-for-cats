a1b5c70153938d42b67b26f0d9b759b48b7ab1ce
crazy baldhead <deepocean.2900@gmail.com>
2024-03-28 12:28:08 -0400

    update readme + title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` 
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Zendesk for Cats",
+  description: "The fastest way to resolve a dispute.",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
