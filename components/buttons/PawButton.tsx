a6749f9f7adf6206d5ce1211e503ef989e677f48
octopus-knight <deepocean.2900@gmail.com>
2024-05-10 21:56:24 -0400

    clicking on paw should stay on same page

--- components/buttons/PawButton.tsx
+++ components/buttons/PawButton.tsx
@@ -4,11 +4,11 @@ import CIcon from "@coreui/icons-react"
 import { cilPaw } from "@coreui/icons"
 import Link from "next/link"
 
-export const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"
+const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"
 
 export default function PawButton() {
   return (
-    <Link href={defaultUrl}>
+    <Link href="/">
       <button className="flex h-auto items-center space-x-2 px-4 py-2 rounded-md border">
         <CIcon icon={cilPaw} />
         <span className="font-semibold">meow meow</span>
